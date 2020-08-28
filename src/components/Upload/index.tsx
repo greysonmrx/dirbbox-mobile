46e6605d1843cde9014824e3ae7f31bcdfff02ba
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-27 22:21:55 -0300

    feat: create upload component

---
+++ src/components/Upload/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {
+  Container
+} from './styles';
+
+const Upload: React.FC<Upload> = ({ name, size, type, created_at }) => {
+  return (
+    <Container />
+  );
+}
+
+export default Upload;
