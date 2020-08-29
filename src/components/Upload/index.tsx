b505ec2b1e0ec014cd7b4d56b18a09f966ab828f
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-29 16:44:16 -0300

    feat: create uploda component
    fix: #1

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
 import {
-  Container
+  Container,
+  Content,
+  IconContainer,
+  UploadIcon,
+  DetailsContainer,
+  UploadName,
+  UploadDate,
+  UploadSize,
 } from './styles';
 
 const Upload: React.FC<Upload> = ({ name, size, type, created_at }) => {
   return (
-    <Container />
+    <Container>
+      <Content>
+        <IconContainer>
+          <UploadIcon name="file-text"/>
+        </IconContainer>
+        <DetailsContainer>
+          <UploadName>{name}</UploadName>
+          <UploadDate>10 de dez de 2020</UploadDate>
+        </DetailsContainer>
+      </Content>
+      <UploadSize>{size}kb</UploadSize>
+    </Container>
   );
 }
 
--- src/components/Upload/styles.ts
+++ src/components/Upload/styles.ts
@@ -1,3 +1,53 @@
 import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
 
-export const Container = styled.View``;
+export const Container = styled.TouchableOpacity`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const Content = styled.View`
+  flex-direction: row;
+`;
+
+export const IconContainer = styled.View`
+  margin-right: 10px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 21px;
+  background-color: #EEF7FE;
+  width: 42px;
+  height: 42px;
+`;
+
+export const UploadIcon = styled(Feather)`
+  font-size: 22px;
+  color: #567DF4;
+`;
+
+export const DetailsContainer = styled.View`
+  justify-content: center;
+`;
+
+export const UploadName = styled.Text`
+  font-size: 15px;
+  font-family: 'Gilroy-Medium';
+  color: #22215B;
+`;
+
+export const UploadDate = styled.Text`
+  font-size: 11px;
+  font-family: 'Gilroy-Regular';
+  color: #22215B;
+  opacity: 0.6;
+`;
+
+export const UploadSize = styled.Text`
+  font-size: 11px;
+  font-family: 'Gilroy-Regular';
+  color: #22215B;
+  opacity: 0.6;
+`;
