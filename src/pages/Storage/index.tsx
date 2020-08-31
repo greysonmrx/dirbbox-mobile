187a660d475bc082f05712d652a3487164f98ac5
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-31 20:28:49 -0300

    feat: create storage page

--- src/pages/Storage/index.tsx
+++ src/pages/Storage/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import Animated from 'react-native-reanimated';
 
-import { Container } from './styles';
+import ProgressBar from '../../components/ProgressBar';
+import Doughnut from '../../components/Doughnut';
+import Header from '../../components/Header';
+
+import formatSize from '../../utils/formatSize';
+
+import { 
+  Container,
+  Scroll,
+  ScrollContainer,
+  StorageDetails,
+  StorageDetailsTitle,
+  StorageSize,
+  StorageTotal,
+  FilesTypesDetails,
+  FileTypeContainer,
+  FirstContent,
+  Ball,
+  FileTypeContent,
+  FileTypeName,
+  FileTypeSize,
+} from './styles';
 
 interface StorageProps {
   style: {
@@ -13,11 +34,96 @@ interface StorageProps {
 }
 
 const Storage: React.FC<StorageProps> = ({ style }) => {
+  const storage = {
+    total: 31000000,
+    remaining: 11160000,
+    data: [ 7.34, 3.30, 2.06, 6.66]
+  }
+
   return (
     <Container 
       style={style}
       as={Animated.View}
-    />
+    >
+      <Header title="Armazenamento"/>
+      <Scroll>
+        <ScrollContainer>
+          <Doughnut data={storage.data}/>
+          <StorageDetails>
+            <StorageDetailsTitle>Disponível</StorageDetailsTitle>
+            <StorageSize>{formatSize(storage.remaining, 2)}</StorageSize>
+            <StorageTotal>Total {formatSize(storage.total)}</StorageTotal>
+          </StorageDetails>
+        </ScrollContainer>
+        <FilesTypesDetails>
+          <FileTypeContainer>
+            <FirstContent>
+              <Ball color="#22215B"/>
+              <FileTypeContent>
+                <FileTypeName>Programação</FileTypeName>
+                <FileTypeSize>7.34 GB</FileTypeSize>
+              </FileTypeContent>
+            </FirstContent>
+            <ProgressBar 
+              color="#22215B"
+              progress={36}
+            />
+          </FileTypeContainer>
+          <FileTypeContainer>
+            <FirstContent>
+              <Ball color="#FFC700"/>
+              <FileTypeContent>
+                <FileTypeName>Imagens</FileTypeName>
+                <FileTypeSize>6.66 GB</FileTypeSize>
+              </FileTypeContent>
+            </FirstContent>
+            <ProgressBar 
+              color="#FFC700" 
+              progress={20}
+            />
+          </FileTypeContainer>
+          <FileTypeContainer>
+            <FirstContent>
+              <Ball color="#4CE364"/>
+              <FileTypeContent>
+                <FileTypeName>Vídeos</FileTypeName>
+                <FileTypeSize>38.66 GB</FileTypeSize>
+              </FileTypeContent>
+            </FirstContent>
+            <ProgressBar 
+              color="#4CE364" 
+              progress={8}
+            />
+          </FileTypeContainer>
+          <FileTypeContainer>
+            <FirstContent>
+              <Ball color="#567DF4"/>
+              <FileTypeContent>
+                <FileTypeName>Documentos</FileTypeName>
+                <FileTypeSize>38.66 GB</FileTypeSize>
+              </FileTypeContent>
+            </FirstContent>
+            <ProgressBar 
+              color="#567DF4" 
+              progress={34}
+            />
+          </FileTypeContainer>
+          <FileTypeContainer>
+            <FirstContent>
+              <Ball color="#FF842A"/>
+              <FileTypeContent>
+                <FileTypeName>Outros</FileTypeName>
+                <FileTypeSize>38.66 GB</FileTypeSize>
+              </FileTypeContent>
+            </FirstContent>
+            <ProgressBar 
+              color="#FF842A" 
+              progress={12}
+            />
+          </FileTypeContainer>
+        </FilesTypesDetails>
+      </Scroll>
+    </Container>
   );
 }
 
--- src/pages/Storage/styles.ts
+++ src/pages/Storage/styles.ts
@@ -1,6 +1,91 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
+interface BallProps {
+  color: string;
+}
 
 export const Container = styled.View`
   flex: 1;
-  background-color: red;
+  background-color: #FFFFFF;
+  align-items: center;
+`;
+
+export const Scroll = styled.ScrollView`
+  flex: 1;
+`;
+
+export const ScrollContainer = styled.View`
+  flex: 1;
+`;
+
+export const StorageDetails = styled.View`
+  width: 100%;
+  padding: 10px 30px 30px;
+  align-items: center;
+`;
+
+export const StorageDetailsTitle = styled.Text`
+  font-size: 20px;
+  font-family: "Gilroy-Regular";
+  color: #22215B;
+  margin-bottom: 8px;
+  text-align: center;
+`;
+
+export const StorageSize = styled.Text`
+  font-size: 24px;
+  font-family: "Gilroy-Bold";
+  color: #22215B;
+  margin-bottom: 8px;
+  text-align: center;
+`;
+
+export const StorageTotal = styled.Text`
+  font-size: 20px;
+  font-family: "Gilroy-Regular";
+  color: #22215B;
+`;
+
+export const FilesTypesDetails = styled.View`
+  padding: 30px;
+  padding-bottom: ${getBottomSpace()}px;
+`;
+
+export const FileTypeContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-bottom: 24px;
+`;
+
+export const FirstContent = styled.View`
+  flex-direction: row;
+`;
+
+export const Ball = styled.View<BallProps>`
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+  background-color: ${({ color }) => color};
+  margin-right: 10px;
+`;
+
+export const FileTypeContent = styled.View`
+  align-items: flex-start;
+`;
+
+export const FileTypeName = styled.Text`
+  font-size: 18px;
+  font-family: 'Gilroy-Medium';
+  color: #22215B;
+  margin-bottom: 5px;
+  line-height: 17px;
+`;
+
+export const FileTypeSize = styled.Text`
+  font-size: 12px;
+  font-family: 'Gilroy-Medium';
+  color: #22215B;
+  opacity: 0.6;
 `;
